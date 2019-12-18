69c56da7970e019381b5bfe07ccd6da949f30a65
YangF0917 <felix.yang0917@gmail.com>
2019-12-18 16:16:06 -0500

    Created intro file and NumPy package

---
+++ NumPy/Intro.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Initializing a NumPy array
+arr = np.array([-1, 2, 5], dtype=np.float32)
+
+# Print the representation of the array
+# print(repr(arr))
+print(arr)
+
+'''
+without the repr, outputs "[-1.  2.  5.]"
+dtype represents the data type for the elements in the array
+'''
