24e39fff35dbdcec4ac593b7145763c682ea3c1d
YangF0917 <felix.yang0917@gmail.com>
2019-12-20 22:37:41 -0500

    Add nonlinear arithmetic functions on arrays

--- NumPy/Math.py
+++ NumPy/Math.py
@@ -17,3 +17,23 @@ print(repr(arr**2))
 # Square root element values
 print(repr(arr**0.5))
 
+def f2c(temps):
+  return (5/9)*(temps-32)
+
+fahrenheits = np.array([32, -4, 14, -40])
+celsius = f2c(fahrenheits)
+print('Celsius: {}'.format(repr(celsius)))
+# f2c just creates a function
+
+# Non-linear arithmetic on Arrays
+arr = np.array([[1, 2], [3, 4]])
+# Exponent base e
+print(repr(np.exp(arr)))
+# Exponent base 2
+print(repr(np.exp2(arr)))
+
+arr2 = np.array([[1, 10], [np.e, np.pi]])
+# Natural logarithm
+print(repr(np.log(arr2)))
+# Base 10 logarithm
+print(repr(np.log10(arr2)))
