0c8c022619df12358ecbb82bb6013284d329cde0
YangF0917 <felix.yang0917@gmail.com>
2019-12-20 22:50:58 -0500

    Add introduction to matrix multiplication

--- NumPy/Math.py
+++ NumPy/Math.py
@@ -46,3 +46,15 @@ print(repr(np.power(3, arr)))
 arr2 = np.array([[10.2, 4], [3, 5]])
 # Raise arr2 to power of each number in arr
 print(repr(np.power(arr2, arr)))
+
+# Matrices
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([-3, 0, 10])
+print(np.matmul(arr1, arr2))
+
+arr3 = np.array([[1, 2], [3, 4], [5, 6]])
+arr4 = np.array([[-1, 0, 1], [3, 2, -4]])
+print(repr(np.matmul(arr3, arr4)))
+print(repr(np.matmul(arr4, arr3)))
+# This will result in ValueError
+# print(repr(np.matmul(arr3, arr3)))
