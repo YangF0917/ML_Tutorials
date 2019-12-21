6658c14dac83b883eec056fd803723511721d3b0
YangF0917 <felix.yang0917@gmail.com>
2019-12-20 22:19:46 -0500

    Add tutorial to array arithmetic

---
+++ NumPy/Math.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Basic arithmetic on arrays
+arr = np.array([[1, 2], [3, 4]])
+# Add 1 to element values
+print(repr(arr + 1))
+# Subtract element values by 1.2
+print(repr(arr - 1.2))
+# Double element values
+print(repr(arr * 2))
+# Halve element values
+print(repr(arr / 2))
+# Integer division (half)
+print(repr(arr // 2))
+# Square element values
+print(repr(arr**2))
+# Square root element values
+print(repr(arr**0.5))
+
