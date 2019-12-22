c3c17be1cbe41eb580ec802296426cea23d59a50
YangF0917 <felix.yang0917@gmail.com>
2019-12-21 23:01:34 -0500

    Add types of distributions (normal, uniform).

--- NumPy/Random.py
+++ NumPy/Random.py
@@ -45,3 +45,18 @@ matrix = np.array([[1, 2, 3],
 np.random.shuffle(matrix)
 print(repr(matrix))
 # Note, this will shuffle only the first dimension
+
+# Distributions
+print(np.random.uniform())
+print(np.random.uniform(low=-1.5, high=2.2))
+print(repr(np.random.uniform(size=3)))
+print(repr(np.random.uniform(low=-3.4, high=5.9,
+                             size=(2, 2))))
+# Generates numbers based on an uniform distribution
+
+print(np.random.normal())
+print(np.random.normal(loc=1.5, scale=3.5))
+print(repr(np.random.normal(loc=-2.4, scale=4.0,
+                            size=(2, 2))))
+# loc represents the mean, scale represents std deviation
+# Random numbers taken using normal distribution
