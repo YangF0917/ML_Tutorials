d927c0130be2af5a9df58e1893b4f645be0676f0
YangF0917 <felix.yang0917@gmail.com>
2019-12-21 23:03:18 -0500

    Complete tutorial on random generation in numpy

--- NumPy/Random.py
+++ NumPy/Random.py
@@ -60,3 +60,12 @@ print(repr(np.random.normal(loc=-2.4, scale=4.0,
                             size=(2, 2))))
 # loc represents the mean, scale represents std deviation
 # Random numbers taken using normal distribution
+
+# Random choice
+colors = ['red', 'blue', 'green']
+print(np.random.choice(colors))
+print(repr(np.random.choice(colors, size=2)))
+print(repr(np.random.choice(colors, size=(2, 2),
+                            p=[0.8, 0.19, 0.01])))
+# p gives the probability of each event occuring (must sum to 1)
+
