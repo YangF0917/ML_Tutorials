2165e352d930d65d9506e40eeaec6f789bacfa31
YangF0917 <felix.yang0917@gmail.com>
2019-12-23 16:24:37 -0500

    Complete numpy array indexing tutorial

--- NumPy/Indexing.py
+++ NumPy/Indexing.py
@@ -33,3 +33,22 @@ print(repr(arr[0:1, 1:]))
 print(repr(arr[0, 1:]))
 # Note that the comma indicates how to splice each dimension respectively
 
+# Argmin and Argmax
+arr = np.array([[-2, -1, -3],
+                [4, 5, -6],
+                [-3, 9, 1]])
+print(np.argmin(arr[0]))
+print(np.argmax(arr[2]))
+print(np.argmin(arr))
+# Returns the index of the largest/smallest element in the array
+# Using this operation on an multi-dimension array will return the index based on the flattened array
+
+arr = np.array([[-2, -1, -3],
+                [4, 5, -6],
+                [-3, 9, 1]])
+print(repr(np.argmin(arr, axis=0)))
+# returns the max row for each column
+print(repr(np.argmin(arr, axis=1)))
+# retuns max column for each row
+print(repr(np.argmax(arr, axis=-1)))
+
