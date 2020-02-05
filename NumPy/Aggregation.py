b6f9131acfd4d0638428aee8654d4ec2d530a260
YangF0917 <felix.yang0917@gmail.com>
2020-02-04 21:08:36 -0500

    Complete aggregation tutorial

--- NumPy/Aggregation.py
+++ NumPy/Aggregation.py
@@ -8,3 +8,24 @@ print(np.sum(arr))
 print(repr(np.sum(arr, axis=0)))
 print(repr(np.sum(arr, axis=1)))
 
+# Cumulative sums
+arr = np.array([[0, 72, 3],
+                [1, 3, -60],
+                [-3, -2, 4]])
+print(repr(np.cumsum(arr)))
+print(repr(np.cumsum(arr, axis=0)))
+print(repr(np.cumsum(arr, axis=1)))
+# Notice that the cumulative sums with axis arguments are summing columns and rows cumulatively
+
+# Concatenating
+arr1 = np.array([[0, 72, 3],
+                 [1, 3, -60],
+                 [-3, -2, 4]])
+arr2 = np.array([[-15, 6, 1],
+                 [8, 9, -4],
+                 [5, -21, 18]])
+print(repr(np.concatenate([arr1, arr2])))
+print(repr(np.concatenate([arr1, arr2], axis=1)))
+print(repr(np.concatenate([arr2, arr1], axis=1)))
+# Each of them concatenate the arrays in a different way
+
