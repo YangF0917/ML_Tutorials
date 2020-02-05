9428fad0c94c847ad2d4833a61c28b13f9979982
YangF0917 <felix.yang0917@gmail.com>
2020-02-04 23:12:04 -0500

    Complete saving and loading tutorial

---
+++ NumPy/SaveData.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+# Saving data to NumPy files:
+arr = np.array([1, 2, 3])
+# Saves to 'arr.npy'
+np.save('arr.npy', arr)
+# Also saves to 'arr.npy'
+np.save('arr', arr)
+
+# Loading from a file
+arr = np.array([1, 2, 3])
+np.save('arr.npy', arr)
+load_arr = np.load('arr.npy')
+print(repr(load_arr))
+
+# Will result in FileNotFoundError since there is no file extension
+# load_arr = np.load('arr')
